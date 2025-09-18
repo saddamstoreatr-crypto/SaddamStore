f5ca87377c42a3eb5a1390a2713370042dcd4a73
saddamstoreatr-crypto <saddamstoreatr@gmail.com>
2025-09-18 06:35:18 -0700

    ouibh

---
+++ app/src/main/java/com/sdstore/auth/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.sdstore.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class AuthActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // A proper implementation would require a layout file.
+        // For now, this activity will be blank to prevent the app from crashing.
+    }
+}
