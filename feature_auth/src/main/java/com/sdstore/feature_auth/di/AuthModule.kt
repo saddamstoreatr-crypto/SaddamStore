72505ff84f01228e8f823516ce5ac984a0466f36
saddamstoreatr-crypto <saddamstoreatr@gmail.com>
2025-09-18 06:52:01 -0700

    ,jcvhn

--- feature_auth/src/main/java/com/sdstore/feature_auth/di/AuthModule.kt
+++ feature_auth/src/main/java/com/sdstore/feature_auth/di/AuthModule.kt
@@ -3,20 +3,20 @@ package com.sdstore.feature_auth.di
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.storage.FirebaseStorage
-import com.sdstore.feature_auth.data.RegisterRepositoryImpl
 import com.sdstore.core.data.repository.RegisterRepository
+import com.sdstore.feature_auth.data.RegisterRepositoryImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
-import javax.inject.Singleton
+import dagger.hilt.android.components.ActivityComponent
+import dagger.hilt.android.scopes.ActivityScoped
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ActivityComponent::class)
 object AuthModule {
 
     @Provides
-    @Singleton
+    @ActivityScoped
     fun provideRegisterRepository(
         auth: FirebaseAuth,
         firestore: FirebaseFirestore,
